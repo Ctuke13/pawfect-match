0d182b4f56c734d9264e33f83fcf9ebe8025c955
Chidi <chidituke@gmail.com>
2025-02-09 13:07:19 -0500

    modify api.ts

--- services/api.ts
+++ services/api.ts
@@ -9,3 +9,31 @@ export const apiClient = axios.create({
         "Content-Type": "application/json"
     }
 })
+
+// Fetch all dog breeds
+export const getBreeds = async (): Promise<string[]> => {
+    const response = await apiClient.get("/dogs/breeds");
+    return response.data;
+};
+
+// Search with filters
+export const searchDogs = async (filters: any) => {
+    const response = await apiClient.get("/dogs/search", { params: filters });
+    return response.data;
+}
+
+// Fetch dogs by ID
+export const getDogsById = async (dogIds: string[]) => {
+    const response = await apiClient.post("/dogs", dogIds);
+    return response.data;
+};
+
+
+// Update Favorite dogs
+export const updateUserFavorites = (favorites: string[]) => {
+    const user = JSON.parse(localStorage.getItem("user") || "{}");
+    if (user) {
+      user.favorites = favorites;
+      localStorage.setItem("user", JSON.stringify(user));
+    }
+}
